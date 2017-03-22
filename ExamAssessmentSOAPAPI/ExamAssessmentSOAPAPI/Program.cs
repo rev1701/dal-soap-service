9e323babc82f165298a038faeab239f060349611
Stephen <stephenoseiowusu199@gmail.com>
2017-03-22 16:45:50 -0400

    new

---
+++ ExamAssessmentSOAPAPI/ExamAssessmentSOAPAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace LMS1701.EA.SOAPAPI
+{
+    public class Program
+    {
+        static void main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(IService1)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " +DateTime.Now.ToString());
+                Console.ReadLine();
+
+            }
+        }
+      
+    }
+}
