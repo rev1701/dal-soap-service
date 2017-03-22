364946613e82933e81ff0ea50a506dfadda5e609
DESKTOP-F28K8BC\pstan <pstan>
2017-03-22 13:42:08 -0400

    Set Up NLog to save log files to C:\Logs\EASOAPAPILogs

---
+++ ExamAssessmentSOAPAPI/ExamAssessmentSOAPAPI/NLogConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NLog;
+
+namespace ExamAssessmentSOAPAPI
+{
+    public class NLogConfig
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+    }
+}
