a42c26dc507230c6f7c7ea4b2f6d71b11a7f3e74
DESKTOP-F28K8BC\pstan <pstan>
2017-03-22 13:38:48 -0400

    Added Unit Test Library, and mocked out unit tests.

---
+++ ExamAssessmentSOAPAPI/SOAPAPIUnitTests/QuestionCategoriesTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS1701.EA.SOAPAPI;
+using NUnit.Framework;
+
+namespace SOAPAPIUnitTests
+{
+    [TestFixture]
+    class QuestionCategoriesTests
+    {
+        [SetUp]
+        protected void SetUpMockData()
+        {
+
+        }
+        [Test]
+        public void ValidCategory()
+        {
+
+        }
+    }
+}
