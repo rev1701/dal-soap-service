5327198f797213a596bfc7b0fbfb79ae5f34807c
Stephen <stephenoseiowusu199@gmail.com>
2017-03-30 13:44:05 -0400

    new commit

--- ExamAssessmentSOAPAPI/ExamassessmentWCF/Service1.svc.cs
+++ ExamAssessmentSOAPAPI/ExamassessmentWCF/Service1.svc.cs
@@ -108,6 +108,7 @@ namespace LMS1701.EA.SOAPAPI
         }
         public List<Question> GetAllQuestions()
         {
+            AutoMapperConfiguration.Configure();
             List<Question> result = new List<Question>();
             var first = from c in db.Question                    
                         select c;
@@ -127,6 +128,7 @@ namespace LMS1701.EA.SOAPAPI
         }
         public List<Answers> GetAnswersQuestion(int Questid)
         {
+            AutoMapperConfiguration.Configure();
             var Question = from c in db.QuestionAnswers
                         where c.QuestionID == Questid
                         select c.AnswerID;
@@ -136,6 +138,7 @@ namespace LMS1701.EA.SOAPAPI
                 var second = from x in db.Answer
                              where x.PKID == Question.ToArray()[k]
                              select x;
+                Answers ans = new Answers();
                 var resulter = Mapper.Map<Answers>(second);
                 i.Add(resulter);
 
