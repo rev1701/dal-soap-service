6ff731ac614151e3adfe158facdebc874e58ad57
DESKTOP-F28K8BC\pstan <pstan>
2017-04-04 12:27:49 -0400

    .

--- ExamAssessmentSOAPAPI/ExamassessmentWCF/DTO/Question.cs
+++ ExamAssessmentSOAPAPI/ExamassessmentWCF/DTO/Question.cs
@@ -21,8 +21,6 @@ namespace LMS1701.EA.SOAPAPI
         public int PKID { get; set; }
         [DataMember]
         public string Description { get; set; }
-        [DataMember]
-        public Correct correct { get; set; }
 
         //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         //  public virtual ICollection<ExamQuestionList> ExamQuestionLists { get; set; }
